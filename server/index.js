a0ae23dc76f1ccb713a6f49cabd38ae2c043bdfa
Shubham Vishwakarma <143716883+shubham8828@users.noreply.github.com>
2025-02-20 21:08:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors(
     
     {
-        origin: ["https://aimps.vercel.app/","http://localhost:5173/"],
+        origin: ["https://aimps.vercel.app"],
         methods: ["POST", "GET","PUT","DELETE"],
         credentials: true
     }
