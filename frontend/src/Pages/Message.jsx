9bb077e0cfda0197a51fda7e0df509c11feb78fd
Shubham Vishwakarma <143716883+shubham8828@users.noreply.github.com>
2025-02-21 10:44:14 +0530

    Update Message.jsx

--- frontend/src/Pages/Message.jsx
+++ frontend/src/Pages/Message.jsx
@@ -32,7 +32,7 @@ const Message = () => {
   const fetchData = async () => {
     setLoading(true)
     try {
-      const usersResponse = await axios.get("http://localhost:4000/api/users", {
+      const usersResponse = await axios.get("https://aimps-server.vercel.app/api/users", {
         headers,
       });
       setUsers(usersResponse.data.users);
@@ -57,7 +57,7 @@ const Message = () => {
 
     axios
       .post(
-        "http://localhost:4000/api/messages",
+        "https://aimps-server.vercel.app/api/messages",
         { sender, receiver },
         { headers }
       )
