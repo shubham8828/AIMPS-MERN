dde2ed034f4adb19e0fc6e5a112a6668ab76137a
Shubham Vishwakarma <skv6621@gmail.com>
2025-03-03 14:14:16 +0530

    profile fixed

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -7,8 +7,17 @@ import "./Profile.css";
 import Spinner from "../Component/Spinner";
 import defaultProfile from "../assets/defaultProfile.png"; // Define a default profile image
 
-const statesOfIndia = [...];
 
+const statesOfIndia = [
+  "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh",
+  "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand",
+  "Karnataka", "Kerala", "Madhya Pradesh", "Maharashtra", "Manipur",
+  "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab",
+  "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura",
+  "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman and Nicobar Islands",
+  "Chandigarh", "Dadra and Nagar Haveli and Daman and Diu", "Delhi",
+  "Jammu and Kashmir", "Ladakh", "Lakshadweep", "Puducherry",
+];
 const Profile = () => {
   const [user, setUser] = useState(null); 
   const [formData, setFormData] = useState({
